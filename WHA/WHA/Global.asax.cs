7ace30f61637e006a64c288ee11255a8fafba5c4
Khurshid Wali <khurshid135@hotmail.com>
2017-02-12 04:17:25 +0500

    connectionString

--- WHA/WHA/Global.asax.cs
+++ WHA/WHA/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -9,12 +10,14 @@ using System.Web.Routing;
 using AutoMapper;
 using WHA.App_Start;
 
+
 namespace WHA
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            
             Mapper.Initialize(c=>c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
