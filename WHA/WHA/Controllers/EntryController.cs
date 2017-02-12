2139ca0701c97086cac133bc8a184e3607bf21b2
Khurshid Wali <khurshid135@hotmail.com>
2017-02-13 02:14:50 +0500

    Entry View

---
+++ WHA/WHA/Controllers/EntryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WHA.Controllers
+{
+    public class EntryController : Controller
+    {
+        // GET: Entry
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
