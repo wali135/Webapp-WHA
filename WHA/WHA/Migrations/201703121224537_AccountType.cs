292c282620581108df907ac8920b5a833a014a06
Khurshid Wali <khurshid135@hotmail.com>
2017-03-20 17:32:23 +0500

    Register option is alloted to admin only.

---
+++ WHA/WHA/Migrations/201703121224537_AccountType.cs
@@ -0,0 +1,16 @@
+namespace WHA.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AccountType : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
